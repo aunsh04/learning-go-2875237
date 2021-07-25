9203149f5dd004e7507924f9d6e59fdf50991871
aunshchaudhari <aunshchaudhari@ccs.neu.edu>
2021-07-25 12:55:26 -0700

    using math package

--- practice/main.go
+++ practice/main.go
@@ -2,10 +2,21 @@ package main
 
 import (
 	"fmt"
+	"math"
 )
 
 func main() {
 
-	fmt.Println("Math")
+	i1, i2, i3 := 12, 45, 68
+	intSum := i1 + i2 + i3
+	fmt.Println("sum:", intSum)
+
+	f1, f2 := 10.2, 15.6
+	floatSum := f1 + f2
+	fmt.Println("Float sum:", math.Round(floatSum))
+
+	radius := 15.2
+	circumference := 2 * math.Pi * radius
+	fmt.Printf("Circumference: %.2f\n", circumference)
 
 }
