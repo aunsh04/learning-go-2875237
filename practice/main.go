0fee7f028d147e3807b617ba336bd9ad3460f91b
aunshchaudhari <aunshchaudhari@ccs.neu.edu>
2021-08-03 22:50:37 -0700

    learning arrays and manipulation

--- practice/main.go
+++ practice/main.go
@@ -6,4 +6,18 @@ import (
 
 func main() {
 	fmt.Println("Arrays")
+
+	var colors = [3]string{"h", "k", "l"}
+	fmt.Println(colors)
+	fmt.Println(len(colors))
+
+	var colors1 [2]string
+	colors1[0] = "j"
+	colors1[1] = "y"
+
+	fmt.Println(colors1)
+
+	var p = 42
+	k := &p
+	fmt.Println(*k)
 }
