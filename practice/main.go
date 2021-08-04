b9a37dcab22b5ce689ef23973025db00ec8044cb
aunshchaudhari <aunshchaudhari@ccs.neu.edu>
2021-08-03 22:33:12 -0700

    learning pointers

--- practice/main.go
+++ practice/main.go
@@ -5,5 +5,17 @@ import (
 )
 
 func main() {
+	anInt := 10
+	p := &anInt
+	fmt.Println("Value of p:", *p)
 	fmt.Println("Pointers")
+
+	value1 := 50
+	p1 := &value1
+	fmt.Println(p1)
+	fmt.Println(*p1)
+
+	*p1 = *p1 * 20
+	fmt.Println(*p1)
+	fmt.Println(value1)
 }
