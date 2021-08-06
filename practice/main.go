bcd94d6fd1323ef8204258d07cf4203308ac944b
aunshchaudhari <aunshchaudhari@ccs.neu.edu>
2021-08-05 23:10:35 -0700

    using slices and sorting

--- practice/main.go
+++ practice/main.go
@@ -2,9 +2,21 @@ package main
 
 import (
 	"fmt"
+	"sort"
 )
 
 func main() {
-	var colors = [3]string{"Red", "Green", "Blue"}
+	var colors = []string{"Red", "Green", "Blue"}
 	fmt.Println(colors)
+	colors = append(colors, "Purple")
+	fmt.Println(colors)
+
+	colors = append(colors[2 : len(colors)-1])
+	fmt.Println(colors)
+
+	var colors1 = []string{"Red", "Green", "Blue"}
+
+	sort.Strings(colors1)
+	fmt.Println(colors1)
+
 }
