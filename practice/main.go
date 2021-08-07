9999189af34de39bc07378aef3b1096d4807b7cb
aunshchaudhari <aunshchaudhari@ccs.neu.edu>
2021-08-06 22:44:51 -0700

    using conditional if statement

--- practice/main.go
+++ practice/main.go
@@ -6,4 +6,16 @@ import (
 
 func main() {
 	fmt.Println("Conditional logic")
+
+	theAnswer := 42
+	var result string
+	if theAnswer < 0 {
+		result = "less than zero"
+	} else if theAnswer == 0 {
+		result = "Equal to zero"
+	} else {
+		result = "Greater than zero"
+	}
+	fmt.Println(result)
+
 }
