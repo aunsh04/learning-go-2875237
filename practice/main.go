b35695e4f02354da48ca889006b861d6b14477c3
aunshchaudhari <aunshchaudhari@ccs.neu.edu>
2021-08-07 15:58:09 -0700

    working with for loops and switch

--- practice/main.go
+++ practice/main.go
@@ -7,4 +7,20 @@ import (
 func main() {
 	colors := []string{"Red", "Green", "Blue"}
 	fmt.Println(colors)
+
+	for i := 0; i < len(colors); i++ {
+		fmt.Println(colors[i])
+	}
+	for i, _ := range colors {
+		fmt.Println(i)
+	}
+
+	i := 1
+	switch i {
+	case 1:
+		fmt.Println("hello")
+		fallthrough
+	case 2:
+		fmt.Println("Hi")
+	}
 }
