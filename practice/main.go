aabcde4b97db1b402010f4d3984bedb96ec03678
aunshchaudhari <aunshchaudhari@ccs.neu.edu>
2021-08-07 18:42:42 -0700

    defining custom functions

--- practice/main.go
+++ practice/main.go
@@ -9,10 +9,18 @@ func main() {
 	fmt.Println(poodle)
 	fmt.Printf("%+v\n", poodle)
 	fmt.Printf("Breed: %v\nWeight: %v\n", poodle.Breed, poodle.Weight)
+
+	poodle.Weigh()
+
 }
 
 // Dog is a struct
 type Dog struct {
 	Breed  string
 	Weight int
+}
+
+// Speak is how the dog speaks
+func (d Dog) Weigh() {
+	fmt.Println(d.Weight)
 }
