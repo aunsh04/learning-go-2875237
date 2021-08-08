4087757b55c01fc4b6938da1743d6179060f4f97
aunshchaudhari <aunshchaudhari@ccs.neu.edu>
2021-08-07 18:16:20 -0700

    working with functions

--- practice/main.go
+++ practice/main.go
@@ -6,4 +6,21 @@ import (
 
 func main() {
 	fmt.Println("Functions")
+	sum := addValues(10, 10)
+	fmt.Println(sum)
+	newSum, newSum1 := addMultipleValues(10, 10, 10)
+	fmt.Println(newSum)
+	fmt.Println(newSum1)
+}
+
+func addValues(value1, value2 int) int {
+	return value1 + value2
+}
+
+func addMultipleValues(values ...int) (int, int) {
+	total := 0
+	for _, val := range values {
+		total += val
+	}
+	return total, 9
 }
