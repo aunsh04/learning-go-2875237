abb31c40431cc99610e33f5b70e922436c61244e
aunshchaudhari <aunshchaudhari@ccs.neu.edu>
2021-08-08 16:03:58 -0700

    reading and writing to files

--- calculator/main.go
+++ calculator/main.go
@@ -2,8 +2,32 @@ package main
 
 import (
 	"fmt"
+	"io"
+	"io/ioutil"
+	"os"
 )
 
 func main() {
 	fmt.Println("Files")
+
+	file, err := os.Create("./fromString.txt")
+	checkError(err)
+	length, err := io.WriteString(file, "Hi My name is Aunsh")
+	checkError(err)
+	fmt.Println(length)
+	defer file.Close()
+	defer readFile("./fromString.txt")
+
+}
+
+func checkError(err error) {
+	if err != nil {
+		panic(err)
+	}
+}
+
+func readFile(fileName string) {
+	data, err := ioutil.ReadFile(fileName)
+	checkError(err)
+	fmt.Println("Text read from:", string(data))
 }
