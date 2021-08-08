e7b6bd6e4e1557655aab59ae04eef59d4fa07f4e
aunshchaudhari <aunshchaudhari@ccs.neu.edu>
2021-08-08 15:19:43 -0700

    testing functions for calculator

--- calculator/main.go
+++ calculator/main.go
@@ -3,33 +3,63 @@ package main
 import (
 	"bufio"
 	"fmt"
-	"math"
 	"os"
 	"strconv"
 	"strings"
 )
 
 func main() {
-	reader := bufio.NewReader(os.Stdin)
 
-	fmt.Print("Value 1: ")
+	in1 := acceptInput("Value 1")
+	in2 := acceptInput("Value 2")
+
+	switch operation := acceptOperation(); operation {
+
+	case "+":
+		fmt.Println("Adding the numbers")
+	case "-":
+		fmt.Println("Subtracting the numbers")
+	default:
+		panic("Invalid operation")
+	}
+
+	fmt.Println(in1 + in2)
+	// fmt.Print("Value 1: ")
+	// input1, _ := reader.ReadString('\n')
+	// float1, err := strconv.ParseFloat(strings.TrimSpace(input1), 64)
+	// if err != nil {
+	// 	fmt.Println(err)
+	// 	panic("Value 1 must be a number")
+	// }
+
+	// fmt.Print("Value 2: ")
+	// input2, _ := reader.ReadString('\n')
+	// float2, err := strconv.ParseFloat(strings.TrimSpace(input2), 64)
+	// if err != nil {
+	// 	fmt.Println(err)
+	// 	panic("Value 2 must be a number")
+	// }
+
+	// sum := float1 + float2
+	// sum = math.Round(sum*100) / 100
+	// fmt.Printf("The sum of %v and %v is %v\n\n", float1, float2, sum)
+}
+
+func acceptInput(prompt string) float64 {
+	fmt.Print(prompt)
+	reader := bufio.NewReader(os.Stdin)
 	input1, _ := reader.ReadString('\n')
 	float1, err := strconv.ParseFloat(strings.TrimSpace(input1), 64)
 	if err != nil {
 		fmt.Println(err)
-		panic("Value 1 must be a number")
+		panic("Value must be a number")
 	}
+	return float1
+}
 
-	fmt.Print("Value 2: ")
-	input2, _ := reader.ReadString('\n')
-	float2, err := strconv.ParseFloat(strings.TrimSpace(input2), 64)
-	if err != nil {
-		fmt.Println(err)
-		panic("Value 2 must be a number")
-	}
-
-	sum := float1 + float2
-	sum = math.Round(sum*100) / 100
-	fmt.Printf("The sum of %v and %v is %v\n\n", float1, float2, sum)
-
+func acceptOperation() string {
+	fmt.Print("Enter operation")
+	reader := bufio.NewReader(os.Stdin)
+	operation, _ := reader.ReadString('\n')
+	return strings.TrimSpace(operation)
 }
