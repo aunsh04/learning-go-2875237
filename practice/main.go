cdb2adc4bcd31d7c747ccf17f9f30c186f07cd2c
aunshchaudhari <aunshchaudhari@ccs.neu.edu>
2021-08-09 00:55:35 -0700

    reading text from web

--- practice/main.go
+++ practice/main.go
@@ -2,10 +2,26 @@ package main
 
 import (
 	"fmt"
+	"io/ioutil"
+	"net/http"
 )
 
 const url = "http://services.explorecalifornia.org/json/tours.php"
 
 func main() {
-	fmt.Println("Network requests")
+
+	resp, err := http.Get(url)
+	if err != nil {
+		panic(err)
+	}
+	fmt.Println("Response body:\n", resp.Body)
+	defer resp.Body.Close()
+
+	bytes, err := ioutil.ReadAll(resp.Body)
+	if err != nil {
+		panic(err)
+	}
+
+	content := string(bytes)
+	fmt.Println(content)
 }
